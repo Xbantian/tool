252b274f9dbc0b21f1aea5407acf09e6551f249e
zhongyi <qiuzhongyi@youzan.com>
2020-08-06 21:24:26 +0800

    格式化配置

--- src/App.js
+++ src/App.js
@@ -1,48 +1,35 @@
-import React from "react";
-import { Menu } from "antd";
-import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
+import React from 'react'
+import { Menu } from 'antd'
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons'
 
-const { SubMenu } = Menu;
+const { SubMenu } = Menu
 
 export default class App extends React.Component {
-  state = {
-    current: "mail",
-  };
+	state = {
+		current: 'mail',
+	}
 
-  handleClick = (e) => {
-    console.log("click ", e);
-    this.setState({ current: e.key });
-  };
+	handleClick = e => {
+		console.log('click ', e)
+		this.setState({ current: e.key })
+	}
 
-  render() {
-    const { current } = this.state;
-    return (
-      <Menu
-        onClick={this.handleClick}
-        selectedKeys={[current]}
-        mode="horizontal"
-        style={{ textAlign: "center" }}
-      >
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" icon={<AppstoreOutlined />}>
-          Navigation Two
-        </Menu.Item>
-        <Menu.Item key="alipay">
-          <a
-            href="https://ant.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
-    );
-  }
+	render() {
+		const { current } = this.state
+		return (
+			<Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal" style={{ textAlign: 'center' }}>
+				<Menu.Item key="mail" icon={<MailOutlined />}>
+					Navigation One
+				</Menu.Item>
+				<Menu.Item key="app" icon={<AppstoreOutlined />}>
+					Navigation Two
+				</Menu.Item>
+				<Menu.Item key="alipay">
+					<a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+						Navigation Four - Link
+					</a>
+				</Menu.Item>
+			</Menu>
+		)
+	}
 }
