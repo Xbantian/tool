eaa47818a7cfad07b70c4185961af55900dd25dd
zhongyi <qiuzhongyi@youzan.com>
2020-08-07 16:01:58 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import StringUtil from 'src/views/string-util/index.js'
 import Layout from 'src/views/layout/index.js'
+import Audit from 'src/views/audit/index.js'
 export default function App() {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route path="/audit">
-            <StringUtil />
-          </Route>
-          <Route path="/string">
-            <div></div>
-          </Route>
           <Route path="/home">
             <Welcome />
+          </Route>
+          <Route path="/audit">
+            <Audit />
+          </Route>
+          <Route path="/string">
+            <StringUtil />
           </Route>
           <Redirect to="/home" />
         </Switch>
---
+++ src/views/audit/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Audit(params) {
+  return <div>123qwe</div>
+}
