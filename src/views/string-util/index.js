872b72aaf326ac8f5826bf21d0f6f4862c23e071
zhongyi <qiuzhongyi@youzan.com>
2020-08-07 17:30:36 +0800

    字符串工具

--- src/views/string-util/index.js
+++ src/views/string-util/index.js
@@ -1,5 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function StringUtil(params) {
-  return <div>123qwe</div>
+import { Input, Row, Col } from 'antd'
+import { camelCase, kebabCase, upperFirst, snakeCase } from '_'
+
+export default function StringUtil() {
+  const [input, setInput] = useState('myTest')
+  const [value1, setValue1] = useState('')
+  const [value2, setValue2] = useState('')
+  const [value3, setValue3] = useState('')
+  const [value4, setValue4] = useState('')
+
+  const setValue = val => {
+    setInput(val)
+    setValue1(kebabCase(val))
+    setValue2(camelCase(val))
+    setValue3(snakeCase(val))
+    setValue4(upperFirst(camelCase(val)))
+  }
+
+  return (
+    <>
+      <Row gutter={[24, 16]}>
+        <Col span={6}>
+          <Input
+            placeholder="输入"
+            value={input}
+            onChange={e => {
+              const {
+                target: { value },
+              } = e
+              setValue(value)
+            }}
+          />
+        </Col>
+      </Row>
+      <Row gutter={[24, 16]}>
+        <Col span={6}>
+          <Input placeholder="kebabCase" value={value1} />
+        </Col>
+        <Col span={6}>
+          <Input placeholder="camelCase" value={value2} />
+        </Col>
+        <Col span={6}>
+          <Input placeholder="snakeCase" value={value3} />
+        </Col>
+        <Col span={6}>
+          <Input placeholder="pascalCase" value={value4} />
+        </Col>
+      </Row>
+    </>
+  )
 }
