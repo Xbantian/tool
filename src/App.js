7ce28c2d852bca45719eb544180e46e466b47f6f
zhongyi <qiuzhongyi@youzan.com>
2020-08-07 12:34:28 +0800

    路由更新

--- src/App.js
+++ src/App.js
@@ -1,66 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams, Redirect } from 'react-router-dom'
 import StringUtil from './views/string-util'
+import Layout from './views/layout'
 
-// export default function App1() {
-//   return <div>123</div>
-// }
 export default function App() {
-  // export function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/about">
-          <StringUtil />
-        </Route>
-        <Route path="/topics">
-          <Topics />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/audit">
+            <StringUtil />
+          </Route>
+          <Route path="/string">
+            <div></div>
+          </Route>
+          <Route path="/home">
+            <Welcome />
+          </Route>
+          <Redirect to="/home" />
+        </Switch>
+      </Layout>
     </Router>
   )
 }
 
-function Home() {
-  return <h2>Home</h2>
-}
-
-function Topics() {
-  let match = useRouteMatch()
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  )
-}
-
-function Topic() {
-  let { topicId } = useParams()
-  return <h3>Requested topic ID: {topicId}</h3>
+function Welcome() {
+  return <h2>Welcome</h2>
 }
--- src/views/layout/index.jsx
+++ src/views/layout/index.jsx
@@ -1,37 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Menu } from 'antd'
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons'
-import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom'
-
-import StringUtil from './views/string-util'
-const { SubMenu } = Menu
-
-export default class App extends React.Component {
-  state = {
-    current: 'mail',
-  }
-
-  handleClick = e => {
-    console.log('click ', e)
-    this.setState({ current: e.key })
-  }
-
-  render() {
-    const { current } = this.state
-    return (
-      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal" style={{ textAlign: 'center' }}>
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          <StringUtil />
+import { SmileOutlined, FileTextOutlined, SelectOutlined } from '@ant-design/icons'
+import { useHistory } from 'react-router-dom'
+export default function Layout(props) {
+  console.log(props)
+  const [current, setCurrent] = useState('home')
+  let history = useHistory()
+  return (
+    <div>
+      <Menu
+        onClick={e => {
+          setCurrent(e.key)
+          history.push(e.key)
+        }}
+        selectedKeys={[current]}
+        mode="horizontal"
+        style={{ textAlign: 'center' }}
+      >
+        <Menu.Item key="home" icon={<SmileOutlined />}>
+          首页
         </Menu.Item>
-        <Menu.Item key="app" icon={<AppstoreOutlined />}>
-          Navigation Two
+        <Menu.Item key="audit" icon={<FileTextOutlined />}>
+          发布单
         </Menu.Item>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
+        <Menu.Item key="string" icon={<SelectOutlined />}>
+          小工具
         </Menu.Item>
       </Menu>
-    )
-  }
+      {props.children}
+    </div>
+  )
 }
